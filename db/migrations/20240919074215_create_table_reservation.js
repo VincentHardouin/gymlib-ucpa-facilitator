602dbfffc82a337c2da8d559adbe934827708ad4
Vincent Hardouin <vincenthardouin@icloud.com>
2024-09-19 17:52:13 +0200

    feat: creta reservations table

---
+++ db/migrations/20240919074215_create_table_reservation.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.createTable('reservations', (table) => {
+    table.string('code').primary();
+    table.string('status').notNullable();
+    table.dateTime('created_at').notNullable().defaultTo(knex.fn.now());
+    table.dateTime('updated_at').notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.dropTable('reservations');
+};
+
+export {
+  down,
+  up,
+};
