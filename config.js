78962e8a351ebc8b186d41013a99324bc02dc0c9
Vincent Hardouin <vincenthardouin@icloud.com>
2024-09-22 11:40:37 +0200

    feat: add config verification

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 import { env } from 'node:process';
+import logger from 'imapflow/lib/logger.js';
+import 'dotenv/config';
 
 function isFeatureEnabled(environmentVariable) {
   return environmentVariable === 'true';
@@ -11,6 +13,7 @@ function buildConfiguration() {
       enabled: isFeatureEnabled(env.LOG_ENABLED),
       logLevel: env.LOG_LEVEL || 'info',
     },
+    cronTime: env.CRON_TIME || '* */5 * * * *',
     gymlib: {
       imapConfig: {
         host: env.GYMLIB_MAIL_RECEIVER_IMAP_HOST,
@@ -29,6 +32,8 @@ function buildConfiguration() {
       },
       searchQuery: JSON.parse(env.UCPA_MAIL_RECEIVER_IMAP_SEARCH_QUERY),
       formInfo: JSON.parse(env.FORM_RESPONSE),
+      formSubmit: isFeatureEnabled(env.FORM_SUBMIT_ENABLED),
+      areaId: env.UCPA_AREA_ID,
     },
     notification: {
       url: env.NOTFICATION_URL,
@@ -39,7 +44,38 @@ function buildConfiguration() {
   if (config.environment === 'test') {
     config.logging.enabled = false;
   }
+
+  if (!verifyConfig(config)) {
+    throw new Error('Invalid config');
+  }
   return config;
+}
+
+function verifyConfig(config) {
+  let allKeysHaveValues = true;
+
+  function checkDeep(object, path) {
+    for (const key in object) {
+      if (Object.prototype.hasOwnProperty.call(object, key)) {
+        const value = object[key];
+
+        const currentPath = path ? `${path}.${key}` : key;
+
+        if (typeof value === 'object' && value !== null) {
+          checkDeep(value, currentPath);
+        }
+
+        else if (value === null || value === undefined) {
+          logger.error(`Key "${currentPath}" does not have a valid value.`);
+          allKeysHaveValues = false;
+        }
+      }
+    }
+  }
+
+  checkDeep(config, '');
+
+  return allKeysHaveValues;
 }
 
 export const config = buildConfiguration();
