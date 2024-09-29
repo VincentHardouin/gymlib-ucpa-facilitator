ba81f37d8cce58aded24707e66bc8d64b763a878
Vincent Hardouin <vincenthardouin@icloud.com>
2024-09-29 18:19:44 +0200

    feat: add pass interface without implementation

---
+++ src/infrastructure/PassInterface.js
@@ -0,0 +1,55 @@
+export class PassInterface {
+  constructor({ passController, authService }) {
+    this.passController = passController;
+    this.authService = authService;
+  }
+
+  register(server) {
+    server.route([
+      {
+        method: 'POST',
+        path: `/v1/devices/{deviceLibraryIdentifier}/registrations/{passTypeIdentifier}/{serialNumber}`,
+        options: {
+          pre: [{ method: (request, h) => { return this.authService.validateFromPass(request, h); } }],
+          handler: async (request, h) => {
+            // cf: https://developer.apple.com/documentation/walletpasses/register-a-pass-for-update-notifications
+          },
+          tags: ['api', 'pass'],
+        },
+      },
+      {
+        method: 'GET',
+        path: '/v1/devices/{deviceLibraryIdentifier}/registrations/{passTypeIdentifier}',
+        options: {
+          handler: async (request, h) => {
+            // cf: https://developer.apple.com/documentation/walletpasses/get-the-list-of-updatable-passes
+            const { passesUpdatedSince } = request.query;
+          },
+          tags: ['api', 'pass'],
+        },
+      },
+      {
+        method: 'GET',
+        path: '/v1/passes/{passTypeIdentifier}/{serialNumber}',
+        options: {
+          pre: [{ method: (request, h) => { return this.authService.validateFromPass(request, h); } }],
+          handler: async (request, h) => {
+            // cf: https://developer.apple.com/documentation/walletpasses/send-an-updated-pass
+          },
+          tags: ['api', 'pass'],
+        },
+      },
+      {
+        method: 'DELETE',
+        path: '/v1/devices/{deviceLibraryIdentifier}/registrations/{passTypeIdentifier}/{serialNumber}',
+        options: {
+          pre: [{ method: (request, h) => { return this.authService.validateFromPass(request, h); } }],
+          handler: async (request, h) => {
+            // cf: https://developer.apple.com/documentation/walletpasses/unregister-a-pass-for-update-notifications
+          },
+          tags: ['api', 'pass'],
+        },
+      },
+    ]);
+  }
+}
